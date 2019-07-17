5b8f817860b130a33caa03fd85346385e7b94c2f
tarasov.a <tarasov.art.ser@gmail.com>
2019-07-17 16:41:24 +0300

    get/set token with cookie

--- store/index.js
+++ store/index.js
@@ -1,16 +1,22 @@
 export const state = () => ({
   error: null
-})
+});
+
+export const actions = {
+  nuxtServerInit({dispatch}) {
+    dispatch('auth/autoLogin');
+  }
+};
 
 export const mutations = {
   setError(state, error) {
-    state.error = error
+    state.error = error;
   },
   clearError(state) {
-    state.error = null
+    state.error = null;
   }
-}
+};
 
 export const getters = {
   error: state => state.error
-}
+};
